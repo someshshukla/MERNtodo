6aeaa30d3ba225949f56fcd5af5bfab64541d3fa
Somesh Shukla <somesh13shukla@gmail.com>
2023-02-12 19:46:19 +0530

    completed the code

--- api/server.js
+++ api/server.js
@@ -7,44 +7,54 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-mongoose.connect("mongodb://127.0.0.1:27017/mern-todo",{
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-})
-    .then(() => console.log(" Connected to DB")).catch
-    (console.error);
+mongoose.connect('mongodb://127.0.0.1:27017/react-todo', {
+	useNewUrlParser: true, 
+	useUnifiedTopology: true 
+}).then(() => console.log("Connected to MongoDB")).catch(console.error);
 
-
+// Models
 const Todo = require('./models/Todo');
 
-app.get('/todos', async ( req,res) => {
-    const todos = await Todo.find();
-    res.json(todos);
+app.get('/todos', async (req, res) => {
+	const todos = await Todo.find();
+
+	res.json(todos);
 });
 
 app.post('/todo/new', (req, res) => {
-    const todo = new Todo({
-            text: req.body.text
-    });
+	const todo = new Todo({
+		text: req.body.text
+	})
 
-    todo.save();
-    res.json(todo);
+	todo.save();
+
+	res.json(todo);
 });
 
-app.delete('/todo/delete/:id', async (req, res) =>{
-    const result = await Todo.findByIdAndDelete(req.params.id);
-    
-    res.json(result);
+app.delete('/todo/delete/:id', async (req, res) => {
+	const result = await Todo.findByIdAndDelete(req.params.id);
+
+	res.json({result});
+});
+
+app.get('/todo/complete/:id', async (req, res) => {
+	const todo = await Todo.findById(req.params.id);
+
+	todo.complete = !todo.complete;
+
+	todo.save();
+
+	res.json(todo);
 })
 
-app.put('/todo/complete/:id', async (req, res) => {
-    const todo = await Todo.findById(req.params.id);
+app.put('/todo/update/:id', async (req, res) => {
+	const todo = await Todo.findById(req.params.id);
 
-    todo.complete = !todo.complete;
+	todo.text = req.body.text;
 
-    todo.save();
-    res.json(todo);
-})
+	todo.save();
 
-app.listen(3001, () => console.log("Server started on port 3001"));
+	res.json(todo);
+});
 
+app.listen(3001);
